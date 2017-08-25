d144d6d4be9f2749dd00db6096b560077ab10218
june520 <1445541517@qq.com>
2017-08-25 13:35:04 +0800

    123

--- src/main/java/com/example/controller/Application.java
+++ src/main/java/com/example/controller/Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Created by whj on 2017/5/27.
  */
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class Application {
 
